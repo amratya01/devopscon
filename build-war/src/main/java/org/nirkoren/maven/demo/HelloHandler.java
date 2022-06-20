eccad068308f97af21ba2dc525124065c4e92e5d
Amr Atya <amratya01@gmail.com>
2022-06-20 16:22:29 +0200

    welcome message

--- build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/nirkoren/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.nirkoren.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hello, is it working from your Laptop?";
+		String hello = "Hello, is it working on your Laptop? Then we ship your laptop to the customer!";
 		return hello;
 	}
 
